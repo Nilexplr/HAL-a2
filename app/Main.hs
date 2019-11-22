b1eca8e20b1b0837330c11482361169091a2e17e
nicolasgarde <nicolas.garde@epitech.eu>
2019-11-22 01:47:34 +0100

    [ADD]: prompt

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,31 @@
 module Main where
 
-import Tokenize
+import Argument
+import Prompt
+import System.Exit
+import System.Environment
+import System.IO
+import Text.Printf
+import Prelude hiding (catch)
+import Control.Exception
+import System.Exit
 
+onAbort e = do
+    let x = show (e :: SomeException)
+    putStrLn $ "\nExit"
 
 main :: IO ()
-main = putStr $ cleanString "toto"
+main = do
+    args <- getArgs
+    case args of
+        [] -> handle onAbort launchPrompt
+
+-- main :: IO ()
+-- main = do
+--     argv <- getArgs
+--     args <- handleArgument argv
+--     case args of
+--         Right   (opt)       -> do
+--                 printCluster (imgCompressor (parseFile c, nbColors opt, convergenceLimit opt) [])
+--         Left    (Invalid)   -> exitWith $ ExitFailure 84
+--         _                   -> exitWith ExitSuccess
---
+++ src/Prompt.hs
@@ -0,0 +1,27 @@
+module Prompt
+    ( launchPrompt
+
+    )
+    where
+
+import Tokenize
+import System.IO
+import Text.Printf
+import Prelude hiding (catch)
+import Control.Exception
+import System.Exit
+import Data.Char
+import Control.Monad
+
+-- displayEval :: 
+
+launchPrompt :: IO()
+launchPrompt = forever $ do
+        putStr "> " >> hFlush stdout
+        out <- getLine
+        if null out
+            then return ()
+            else catch (putStrLn $ out) handler
+                where
+                    handler :: SomeException -> IO ()
+                    handler ex = putStrLn $ "***ERROR : " ++ show ex
