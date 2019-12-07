35c81ba62d5ade5e3a1eb6f7f4dbcfe1dc37fcaf
Andy Begin <Osmonoz@localhost.localdomain>
2019-12-07 16:48:06 +0100

    [ADD]: findop function, parse operand expr

--- src/Parser.hs
+++ src/Parser.hs
@@ -9,3 +9,34 @@ import Tokenize
 type Parser a = [Token] -> Maybe(a, [Token])
 
 data Expr = KeyWord String
+            | Function Expr
+
+data OpExpr = AddExpr [Expr]
+            | SubExpr [Expr]
+            | DivExpr [Expr]
+            | MulExpr [Expr]
+            | ModExpr [Expr]
+    deriving Show
+
+-- data Op = ADD
+--         | SUB
+--         | MUL
+--         | DIV
+--         | MOD
+--     deriving Show
+
+parseExpr :: Parser [Expr]
+parseExpr _ -> Nothing
+
+findOp :: [TokenOp] -> [OpExpr] -> Parser OpExpr
+findOp [] _ _ -> Nothing
+findOp _ [] _ -> Nothing
+findOp _ _ [] -> Nothing
+findOp (x:xs) (y:ys) s@(z:zs)   | z == x = just(y, zs)
+                                | z /= x = findOp xs ys s
+findOp _ _ _ -> Nothing
+
+parseOpExpr :: Parser OpExpr
+parseOpExpr [] = Nothing
+parseOpExpr s@(x:xs)
+
