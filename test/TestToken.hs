ec4c492afa9a46a8da90dfc81f6c9229dfbe4c2b
nicolasgarde <nicolas.garde@epitech.eu>
2019-12-19 07:33:27 +0100

    [ADD] Unit Test Token

---
+++ test/TestToken.hs
@@ -0,0 +1,14 @@
+module TestToken (testToken) where
+
+import Test.QuickCheck
+import Tokenize
+import Control.Exception
+
+testToken :: IO ()
+testToken = do
+    quickCheck((stringToToken "+-()/^*") == [ TokenOpen
+                                            , TokenClose
+                                            ])
+--
+    quickCheck(stringToToken "2" == [Number 2])
+    quickCheck(stringToToken "toto" == [Word "toto"])
